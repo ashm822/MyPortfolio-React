0927056781d3561c077d7d8c813721545be76a78
Ashraf Madina <ashraf.madina@asset-map.com>
2020-06-21 00:14:39 -0400

    const port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
-
+const PORT = process.env.PORT || 3000;
 
 ReactDOM.render(
   <React.StrictMode>
